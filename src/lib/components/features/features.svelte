0f298110379b6da05b4806e8d2d03df331c5e1b4
ali elmalki <124408599+aliel0malki@users.noreply.github.com>
2023-08-25 09:41:20 +0300

    Update features.svelte

--- src/lib/components/features/features.svelte
+++ src/lib/components/features/features.svelte
@@ -2,7 +2,7 @@
 	import Card from "./card.svelte"
 </script>
 
-	<h1 class="text-center mt-[5rem] font-black text-3xl text-primary p-0 m-0">Why Us?</h1>
+	<h1 class="text-center mt-[5rem] font-black text-3xl text-primary p-0 m-0">Services</h1>
 
 <div class="flex bg-[#333] mt-6 lg:px-[8rem] flex-wrap justify-center">
 
